8699b6a13068526115bcee8a0a10ca5ad68333ec
Alison <alison.bayona@gmail.com>
2023-07-10 13:08:47 -0400

    update

--- ai-poll-client/src/App.js
+++ ai-poll-client/src/App.js
@@ -5,7 +5,7 @@ import HomePage from '../pages/HomePage';
 import LoginPage from '../pages/LoginPage';
 import PollPage from '../pages/PollPage';
 import SignupPage from '../pages/SignupPage';
-import NavBar from '../components/NavBar';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
